0cbfcfa9b2593d6d9cf01bdbf3ad4fa53497be6a
PiotrKopec10 <91895479+PiotrKopec10@users.noreply.github.com>
2022-06-19 21:31:27 +0200

    COMMIT1

---
+++ MOJASTRONAPHP1COMMIT/Template/__banner-ads.php
@@ -0,0 +1,9 @@
+<!--     Reklama   -->
+<section id="banner_adds">
+    <div class="container py-5 text-center">
+        <img src="../assets/nikereklama.jfif" alt="banner1" class="img-fluid">
+        <img src="../assets/adidasreklama.png" alt="banner1" class="img-fluid">
+    </div>
+</section>
+
+<!--    !Reklama    -->
---
+++ MOJASTRONAPHP1COMMIT/Template/__blogs.php
@@ -0,0 +1,18 @@
+<!--Blogi aka Pisza o nas aka Najnowsze Informacje-->
+<section id="blogs">
+    <div class="container py-4">
+        <h4 class="font-rubik font-size-20">Najnowsze Informacje</h4>
+        <hr>
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="card border-0 font-rale mr-5"style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Nadchodzące Premiery Butów</h5>
+                    <img src="../assets/kubotalogo.png" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Niesamowita Premiera Klapek Kubota! Spiesz sie oferta mocno ograniczona</p>
+                    <a href="#" class="color-second text-left">Przejdź Dalej</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--  !Blogi aka Pisza o nas aka News   -->
